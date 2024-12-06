3236dd946702e761528689de42b5a01cfac37faf
MohBrilliantAh <brilliantmoch@gmail.com>
2024-12-06 23:50:13 +0700

    Add files via upload

---
+++ storage/framework/views/9d158369081fb6bbb5d103cd7ed6db94.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('content'); ?>
+<?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Tugas Kuliah Semester 3\Pengembangan Aplikasi Website\Praktikum Laravel\Modul_14\resources\views/home.blade.php ENDPATH**/ ?>
